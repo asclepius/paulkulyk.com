27f7d62c8b8e5e857db2c16d5b7a1896701dd301
Jason Rudolph <github@jasonrudolph.com>
2009-07-28 21:03:01 -0400

    Update to latest version of Google Analytics code

--- javascripts/google_analytics.js
+++ javascripts/google_analytics.js
@@ -1,10 +1,7 @@
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 
-var pageTracker;
-setTimeout('startGA();', 500);
-function startGA() {
-  pageTracker = _gat._getTracker("UA-4605088-1");
-  pageTracker._initData();
+try{ 
+  var pageTracker = _gat._getTracker("UA-4605088-1");
   pageTracker._trackPageview();
-}
+} catch(err) {} 
